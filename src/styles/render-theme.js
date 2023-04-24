33ddf9690c5f0bd58a7f5c62db5721ea8d21acb9
HenryCavalcant3 <henrycavancante@gmail.com>
2023-04-24 16:04:03 -0300

    'modelo-base-react'

---
+++ src/styles/render-theme.js
@@ -0,0 +1,6 @@
+import { MyThemeProvider } from './MyThemeProvider';
+import { render } from '@testing-library/react';
+
+export const renderTheme = (children) => {
+  return render(<MyThemeProvider>{children}</MyThemeProvider>);
+};
